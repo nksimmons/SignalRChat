290b82a49a2534a0cae735a8d934c79157d4597c
Nick Simmons <NSimmons@council.bbb.org>
2016-08-09 10:06:41 -0500

    Finishing up

--- SignalRChat/Scripts/Custom/chat.js
+++ SignalRChat/Scripts/Custom/chat.js
@@ -1,20 +1,13 @@
 ﻿$(function() {
-
     showLoginSectionOnClick();
-
     var chat = $.connection.chatHub;
-
-    chat.client.manipulateDOMForLoggedInStatus = function () {
+    chat.client.manipulateDOMForLoggedInStatus = function() {
         $("#loginnavv").text("Logout").attr("data-toggle", "");
         $("#application").show();
         $("#discussioncontainer").show();
         $("#login-status").val("1");
-    }
-
-    $("#username").enterKey(function() {
-        $("#login").trigger("click");
-    });
-
+    };
+    $("#username").enterKey(function() { $("#login").trigger("click"); });
     chat.client.sendMessage = function(name, message) {
         var encodedName = $("<div />").text(name).html();
         var encodedMsg = $("<div />").text(message).html();
@@ -22,37 +15,30 @@
         var objDiv = document.getElementById("discussioncontainer");
         objDiv.scrollTop = objDiv.scrollHeight;
     };
-
-    chat.client.showConnectedClients = function (name) {
+    chat.client.showConnectedClients = function(name) {
         var encodedName = $("<div />").text(name).html();
         $("#client-list").append("<p class='client-name' id='client-name-" + encodedName + "'>" + encodedName + "</p>");
     };
-
-    chat.client.alertAtMaxCapacity = function () {
+    chat.client.alertAtMaxCapacity = function() {
         $.connection.hub.stop();
         alert("We are at max capacity. Please try again later.");
         window.location.reload(true);
     };
-
     chat.client.removeDisconnectedClientFromClientList = function(nameOfClient) {
         var clientNameDomId = "#client-name-" + nameOfClient;
         $(clientNameDomId).remove();
     };
-
     $("#message").focus();
-
-    $.connection.hub.start().done(function() {
-        $("#message").enterKey(function() {
-            chat.server.send($("#username").val(), $("#message").val());
-            $("#message").val("").focus();
+    $.connection.hub.start()
+        .done(function() {
+            $("#message")
+                .enterKey(function() {
+                    chat.server.send($("#username").val(), $("#message").val());
+                    $("#message").val("").focus();
+                });
+            $("#login").click(function() { chat.server.login($("#username").val()); });
         });
-
-        $("#login").click(function() {
-            chat.server.login($("#username").val());
-        });
-    });
 });
-
 var showLoginSectionOnClick = function() {
     $("#loginnavv")
         .click(function() {
@@ -66,14 +52,14 @@ var showLoginSectionOnClick = function() {
             };
         });
 };
-
 $.fn.enterKey = function(fnc) {
     return this.each(function() {
-        $(this).keypress(function(ev) {
-            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
-            if (keycode == "13") {
-                fnc.call(this, ev);
-            }
-        });
+        $(this)
+            .keypress(function(ev) {
+                var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+                if (keycode == "13") {
+                    fnc.call(this, ev);
+                }
+            });
     });
 };
